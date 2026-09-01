1b565962ec1aff47bc336227ccb5b62cf0ab616c
YO <user@USERs-MacBook-Pro.local>
2026-09-01 12:05:18 +0530

    chore: remove Daily Motivational Pulse section from website home page

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -5,7 +5,6 @@ import Hero from '@/components/sections/Hero'
 import About from '@/components/sections/About'
 import Services from '@/components/sections/Services'
 import WhyChoose from '@/components/sections/WhyChoose'
-import DynamicQuotes from '@/components/sections/DynamicQuotes'
 import Videos from '@/components/sections/Videos'
 import Testimonials from '@/components/sections/Testimonials'
 import Stats from '@/components/sections/Stats'
@@ -19,7 +18,6 @@ export default function HomePage() {
       <About />
       <Services />
       <WhyChoose />
-      <DynamicQuotes />
       <Videos />
       <Testimonials />
       <Stats />
